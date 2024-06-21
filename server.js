1820103c78fa9bd8c98ae510900bd1e68cb6ab98
BaileyBeck <baileybk2@gmail.com>
2024-06-21 12:48:24 -0400

    got MongoDB to log and store created fruits

--- server.js
+++ server.js
@@ -14,9 +14,30 @@ mongoose.connection.on("connected", () => {
 // Import the Fruit model
 const Fruit = require("./models/fruit.js");
 
+app.use(express.urlencoded({ extended: false }));
+
 // GET /
 app.get("/", async (req, res) => {
   res.render("index.ejs");
+});
+
+// GET /fruits/new
+app.get("/fruits/new", (req, res) => {
+  res.render("fruits/new.ejs");
+  // lesson has a "forward slash in front of the fruits route above and makes an error, just remove the forward slash in front of fruits "
+});
+
+// server.js
+
+// POST /fruits
+app.post("/fruits", async (req, res) => {
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+  await Fruit.create(req.body);
+  res.redirect("/fruits/new");
 });
 
 app.listen(3000, () => {
